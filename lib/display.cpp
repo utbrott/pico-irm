8ac319354deba251a5c4a11c650977f368053d0b
utbrott <rosinskimk@gmail.com>
2023-01-23 00:58:34 +0100

    refactor: indentation width

--- lib/display.cpp
+++ lib/display.cpp
@@ -2,16 +2,16 @@
 
 void display::clear(pico_ssd1306::SSD1306 *display)
 {
-  display->clear();
-  display->sendBuffer();
+    display->clear();
+    display->sendBuffer();
 }
 
 void display::drawText(pico_ssd1306::SSD1306 *display, const char *text, uint8_t col, uint8_t row)
 {
-  col = col * 8;
-  row = row * 8;
+    col = col * 8;
+    row = row * 8;
 
-  drawText(display, font_8x8, text, col, row, pico_ssd1306::WriteMode::ADD);
+    drawText(display, font_8x8, text, col, row, pico_ssd1306::WriteMode::ADD);
 
-  display->sendBuffer();
+    display->sendBuffer();
 }
